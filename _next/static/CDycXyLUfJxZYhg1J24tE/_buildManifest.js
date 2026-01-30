8d9a262e4a5c088461d7b2995cb23c274a3555bb
K\vilor <viloria2124@gmail.com>
2026-01-30 12:12:56 +0800

    - update

--- _next/static/CDycXyLUfJxZYhg1J24tE/_buildManifest.js
+++ _next/static/CDycXyLUfJxZYhg1J24tE/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/titojet-site//_next/:path+",
+        "destination": "/titojet-site/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
